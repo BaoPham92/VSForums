cd51fd6afc3ecdef88e5d048a91008a980f1373b
Bao Cong Pham <BaoPham92@yahoo.com>
2017-06-14 19:26:45 -0400

    crud halfway for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,6 @@
 class CommentsController < ApplicationController
-
-	def index
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(comment_params)
-		@comment.user_id = current_user.id
-
-		if @comment.save
-			redirect_to post_path(@post)
-		else
-			render 'new'
-		end
-	end
+	before_action :find_post
+		before_action :find_comment, only: [:edit, :update, :destroy]
 
 	def show
 	end
@@ -20,14 +10,25 @@ class CommentsController < ApplicationController
 	end
 
 	def create
-		
+			@comment = @post.comments.create(comment_params)
+				@comment.user_id = current_user.id
+
+		if @comment.save
+			redirect_to post_path(@post)
+		else
+			render 'new'
+		end
 	end
 
 	def edit
 	end
 
 	def update
-		
+		if @comment.update(comment_params)
+			redirect_to post_path(@post)
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
@@ -42,6 +43,10 @@ class CommentsController < ApplicationController
 	end
 
 	def find_post
-		
+		@post = Post.find(params[:post_id])
+	end
+
+	def find_comment
+		@comment = @post.comments.find(params[:id])
 	end
 end
