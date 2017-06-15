b5fb27cd14f494490acda5b744c5e39c59d08866
Bao Cong Pham <BaoPham92@yahoo.com>
2017-06-15 11:48:04 -0400

    added rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from NoMethodError, with: :method_error
+
+  private
+  def record_not_found
+    redirect_to root_url, notice: "BRO. THIS DOESN'T EXIST."
+  end
+
+  def method_error
+    redirect_to root_url, notice: "Error. Must sign in if not working."
+  end
 end
